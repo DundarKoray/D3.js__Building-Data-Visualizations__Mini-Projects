9d03ece58307c60b678948f547510cab68cdd51e
Koray Dundar <koraydundar_40@hotmail.com>
2020-02-03 22:22:02 +0200

    practiced the enter selection, DOM, Virtual DOM

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const someData = [
 
 const svg = d3.select('svg');
 
-const rect = svg.selectAll('rect')
+// joining data to rects
+const rects = svg.selectAll('rect')
     .data(someData)
+
+
+// add attrs to rects already in the DOM
+rects
     .attr("width", (d, i, n) => d.width)
     .attr("height", d => d.height)
     .attr('fill', d => d.fill);
 
-// console.log(rect)
+// append the enter selection to DOM
+rects.enter()
+    .append('rect')
+    .attr("width", (d, i, n) => d.width)
+    .attr("height", d => d.height)
+    .attr('fill', d => d.fill);
