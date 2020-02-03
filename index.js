a10d1ebab557d8c537f706d762d9099216156937
Koray Dundar <koraydundar_40@hotmail.com>
2020-02-03 21:59:26 +0200

    joined data for multiple elements

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const someData = [
-    {
-        width: 200,
-        height: 100, 
-        fill: 'purple'
-    }
+    {width: 200, height: 100, fill: 'purple'},
+    {width: 100, height: 60, fill: 'pink'},
+    {width: 50, height: 30, fill: 'red'}
 ];
 
 const svg = d3.select('svg');
 
-const rect = svg.select('rect')
+const rect = svg.selectAll('rect')
     .data(someData)
     .attr("width", (d, i, n) => d.width)
     .attr("height", d => d.height)
