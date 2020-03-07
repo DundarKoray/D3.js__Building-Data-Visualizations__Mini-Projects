c33db43d571427d522682c35163d9fe6f4b1c9e6
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-07 17:07:56 +0200

    rotated xAxisGroup

--- index.js
+++ index.js
@@ -148,11 +148,15 @@ d3.json('./menu.json').then(someData => {
 
     // create and call the axes
     const xAxis = d3.axisBottom(x)
+
     const yAxis = d3.axisLeft(y)
      .ticks(3)
-     .tickFormat(d => d + ' orders')
+     .tickFormat(d => d + ' orders');
 
     xAxisGroup.call(xAxis)
     yAxisGroup.call(yAxis)
 
+    xAxisGroup.selectAll('text')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
 })
