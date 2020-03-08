16951dcdafd11f44d4b51fefbfbdeed8f1c7e39e
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-08 11:47:16 +0200

    completed realtime updating

--- index.js
+++ index.js
@@ -161,7 +161,24 @@ db.collection('dishes').onSnapshot(res => {
 
     res.docChanges().forEach(change => {
 
-        console.log(change.doc.data());
+        const doc = {...change.doc.data(), id: change.doc.id}
+        
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id)
+                data[index] = doc
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id)
+                break;
+            default:
+                break;
+        }
+
+        console.log(change);
 
     });
 
