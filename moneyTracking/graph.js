6c1edacade841a114cf8ddf426ea32e2af0c06dc
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-11 13:00:54 +0200

    created switch for real-time data update

--- moneyTracking/graph.js
+++ moneyTracking/graph.js
@@ -35,11 +35,29 @@ const arcPath = d3.arc()
 /////////////////////////////////
 //** FIRESTORE CONNECTION **//
 
+let data = [];
+
 db.collection('expenses').onSnapshot(res => {
     
     res.docChanges().forEach(change => {
 
         const doc = {...change.doc.data(), id: change.doc.id};
+        // console.log(doc)
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id)
+                break;
+            default:
+                break;
+        }
 
     })
 
