485a83b2cd5831ec16f3d6d14f4db8ac58b9c465
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-11 11:54:34 +0200

    created pie chart dimensions and center of it

---
+++ moneyTracking/graph.js
@@ -0,0 +1,13 @@
+//** PIE CHART **//
+
+// dimensions
+const dims = { height: 300, width: 300, radius: 150};
+// center of the pie chart (+5 gives some breathing room)
+const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5)} 
+
+/////////////////////////////////
+
+
+
+
+
