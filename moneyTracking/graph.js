281a0caa8512be64e44cd4cc89125acd53ef77b4
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-11 12:17:20 +0200

    setted up pie chart

--- moneyTracking/graph.js
+++ moneyTracking/graph.js
@@ -8,12 +8,28 @@ const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5) } ;
 
 /////////////////////////////////
 //** SVG CONTAINER **//
+
+// creating svg
 const svg = d3.select('.canvas')
     .append('svg')
     .attr('width', dims.width + 150) // +150 to give some breathing room
     .attr('height', dims.height + 150) // +150 to give some breathing room
 
+// centering graph in svg
+const graph = svg.append('g')
+    .attr('transform', `translate(${cent.x}, ${cent.y})`);
 
 
+// creating pie
+const pie = d3.pie()
+    .sort(null)
+    .value(d => d.cost)
 
 
+const angles = pie([
+    { name: 'rent', cost: 500 },
+    { name: 'bills', cost: 300 },
+    { name: 'gaming', cost: 200 }
+])
+
+console.log(angles)
