88380b356e7fc393c750a0d6a49ce7a75a0dad19
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-20 09:59:33 +0200

    selected all DOM elements

---
+++ fitnessTracker/index.js
@@ -0,0 +1,6 @@
+// DOM elements
+const btns = document.querySelector('button');
+const form = document.querySelector('form');
+const formAct = document.querySelector('form span');
+const input = document.querySelector('input');
+const error = document.querySelector('.error')
