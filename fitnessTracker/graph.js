18f1e0a0a864e3550e6f2f57afc5edaebbe7590d
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-24 11:39:54 +0200

    completed line path generator and sorted data

--- fitnessTracker/graph.js
+++ fitnessTracker/graph.js
@@ -24,13 +24,31 @@ const xAxisGroup = graph.append('g')
 const yAxisGroup = graph.append('g')
     .attr('class', 'y-axis');
 
+// d3 line path generator
+const line = d3.line()
+    .x(function(d){return x(new Date(d.date))})
+    .y(function(d){return y(d.distance)});
+
+// line path element
+const path = graph.append('path'); 
+
 const update = (data) => {
     // console.log(data);
     data = data.filter(item => item.activity == activity);
 
+    // sort data based on date objects
+    data.sort((a,b) => new Date(a.date) - new Date(b.date));
+    
     // set scale domains (extent find the lowest and the highest value)
     x.domain(d3.extent(data, d => new Date(d.date)));
     y.domain([0, d3.max(data, d => d.distance)]);
+
+    // update path data
+    path.data([data])
+        .attr('fill', 'none')
+        .attr('stroke', '#00bfa5')
+        .attr('stroke-width', 2)
+        .attr('d', line)
 
     // create circles for objects
     const circles = graph.selectAll('circle')
