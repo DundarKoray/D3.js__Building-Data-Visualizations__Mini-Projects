648398a80462a9ae44850005869ca20554f4b178
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-09 11:06:56 +0300

    connected firestore and fixed popup behavier

--- corporationTreeDiagram/index.js
+++ corporationTreeDiagram/index.js
@@ -1,2 +1,26 @@
 const modal = document.querySelector('.modal');
 M.Modal.init(modal)
+
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const parent = document.querySelector('#parent');
+const department = document.querySelector('#department');
+
+form.addEventListener('submit', e => {
+    // this prevents refreshing page
+    e.preventDefault();
+
+    // this saves data in firestore
+    db.collection('employees').add({
+        name: name.value,
+        parent: parent.value,
+        department: department.value
+    });
+
+    // this closes the popup window
+    let instance = M.Modal.getInstance(modal)
+    instance.close();
+
+    // this resets all the values on form back to blank after submitting
+    form.reset();
+})
