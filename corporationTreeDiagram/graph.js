181ce618d3104d50fad091215add57cd3a543806
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-10 10:24:12 +0300

    created enter node groups, appended rects to enter

--- corporationTreeDiagram/graph.js
+++ corporationTreeDiagram/graph.js
@@ -36,7 +36,15 @@ const update = (data) => {
     const enterNodes = nodes.enter()
         .append('g')
             .attr('class', 'node')
-            .attr('transform', d => `translate(${d.x}, ${d.y})`)
+            .attr('transform', d => `translate(${d.x}, ${d.y})`);
+
+    // append rects to enter nodes
+    enterNodes.append('rect')
+        .attr('fill', '#aaa')
+        .attr('stroke', '#555')
+        .attr('stroke-width', 2)
+        .attr('height', 50)
+        .attr('width', d => d.data.name.length * 20) // coz everyname is different 
 };
 
 
